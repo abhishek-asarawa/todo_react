69c203e1dfa705c2a41bf48a07990dec474d22dc
Abhishek Asarawa <abhishek@saniot.in>
2021-12-25 12:34:28 +0530

    adding logic to fetch board data

--- src/views/Boards/Board.js
+++ src/views/Boards/Board.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { Divider } from 'semantic-ui-react';
+import axios from 'axios';
+import isArray from 'lodash/isArray';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setBoards } from '../../redux/actions/board';
+import { openSnack } from '../../redux/actions/snack';
+import BoardTable from './components/Table';
 import Toolbar from './components/Toolbar';
 
+const endpoint = `${process.env.REACT_APP_URL}/boards?task-aggregates=required`;
+
+const columns = [
+  [
+    { id: 'title', textAlign: 'left', name: 'Board Name' },
+    { id: 'createdAt', textAlign: 'left', name: 'Created On' }
+  ],
+  [
+    { id: 'tasks.completed', textAlign: 'center', name: 'Completed' },
+    { id: 'tasks.pending', textAlign: 'center', name: 'Pending' },
+    { id: 'tasks.total', textAlign: 'center', name: 'Total' }
+  ]
+];
+
 const Boards = () => {
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boardsReducer);
+
+  const fetchBoards = useCallback(async () => {
+    try {
+      const response = await axios.get(endpoint);
+
+      if (response.status === 200) {
+        const { data } = response.data;
+
+        if (isArray(data)) dispatch(setBoards(data));
+      }
+    } catch (err) {
+      console.error(err);
+      dispatch(openSnack('Facing error in getting boards.'));
+    }
+    //eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    fetchBoards();
+  }, [fetchBoards]);
+
   return (
     <div>
       <Toolbar />
-      <Divider />
-      <h1>Boards</h1>
+      <BoardTable columns={columns} rows={boards} />
     </div>
   );
 };
---
+++ src/views/Boards/components/Table.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import map from 'lodash/map';
+import { Table } from 'semantic-ui-react';
+
+const getNestedValue = (obj, path) => {
+  const [first, ...rest] = path.split('.');
+  const data = obj[first];
+  if (rest.length === 0) return data;
+  return getNestedValue(data, rest.join('.'));
+};
+
+const BoardTable = ({ columns, rows }) => {
+  return (
+    <Table celled structured striped selectable>
+      <Table.Header>
+        {map(columns, (col, index) => {
+          return (
+            <Table.Row key={`header_${index}`}>
+              {map(col, (cell) => (
+                <Table.HeaderCell
+                  key={cell.id}
+                  rowSpan="2"
+                  textAlign={cell.textAlign ? cell.textAlign : 'left'}
+                >
+                  {cell.name}
+                </Table.HeaderCell>
+              ))}
+              {index === 0 && (
+                <Table.HeaderCell key="tasks" colSpan="3" textAlign="center">
+                  Tasks
+                </Table.HeaderCell>
+              )}
+            </Table.Row>
+          );
+        })}
+      </Table.Header>
+
+      <Table.Body>
+        {map(rows, (row, index) => {
+          return (
+            <Table.Row key={`row_${index}`}>
+              {map([...columns[0], ...columns[1]], (cell, col_index) => {
+                return (
+                  <Table.Cell
+                    key={`${col_index}_${index}`}
+                    textAlign={cell.textAlign ? cell.textAlign : 'left'}
+                  >
+                    {getNestedValue(row, cell.id)}
+                  </Table.Cell>
+                );
+              })}
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default BoardTable;
